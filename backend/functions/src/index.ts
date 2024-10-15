6b35aa2b6ef02945400152d8224303b5d0807a38
Killian <ksowa@outlook.fr>
2024-10-15 10:13:24 +0200

    fix: ready

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,3 +2,4 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export {analyzeSolution} from './ai-survivor/analyze-solution'
+export {analyzePhoto} from './ethylotest/analyze-photo'
